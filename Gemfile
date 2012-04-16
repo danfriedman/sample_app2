ce8fb28ec341fe7bc0bcb224f12d2e57e24476ed
Dan Friedman <dnf1991@gmail.com>
2012-04-16 13:55:05 -0400

    revert to 692569e6644c8b5b3b1bce548760967ca6186452

--- Gemfile
+++ Gemfile
@@ -10,7 +10,6 @@ gem 'gravatar_image_tag', '1.0.0.pre2'
 
 group :development do
   gem 'rspec-rails', '2.6.1'
-  gem 'annotate', '2.4.0'
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20120414154315) do
 
   create_table "users", :force => true do |t|
@@ -19,11 +18,8 @@ ActiveRecord::Schema.define(:version => 20120414154315) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-
     t.string   "encrypted_password"
     t.string   "salt"
   end
+
 end
